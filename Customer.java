1f58bce2915b7d3b28860442254a03d2b61421f5
Emahoney2 <36182779+Emahoney2@users.noreply.github.com>
2018-04-09 23:30:36 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -35,14 +35,26 @@ public class Customer implements Comparable<Customer> {
     public Bank getBank () {
         return null;  // Stub
     }
-
+    @Test
+    public void getBank() {
+    	bank n = new bank();
+    	n.setbank(1);
+    	Assert.assertEquals(1, n.getBank());
+    }
     /** Getter for customer's ID
      * @return The customer's ID
      */
     public String getCustomerId () {
         return null;  // Stub
     }
-
+    
+    @Test
+    public void getCustomerId()
+    {
+    	customerid n = new customerid();
+    	n.setcustomerId(1);
+    	Assert.assertEquals(1, n.getcustomerId());
+    }
     /** Getter for the customer's last name
      * @return The customer's last name
      */
@@ -50,6 +62,12 @@ public class Customer implements Comparable<Customer> {
         return null;  // Stub
     }
 
+    @Test
+    public voidStringLastName() {
+    	String description = "";
+    	assertEquals("", expected);
+    	
+    }
     /** Getter for the customer's first name
      * @return The customer's first name
      */
@@ -58,6 +76,12 @@ public class Customer implements Comparable<Customer> {
         return null;  // Stub
     }
 
+    @Test
+    public voidStringFirstName() {
+    	String description = "";
+    	assertEquals("", expected);
+    	
+    }
     /** Returns a read-only SortedSet of the customer's active
      *  accounts (if any)
      *
@@ -67,7 +91,12 @@ public class Customer implements Comparable<Customer> {
     public SortedSet<Account> getCustomerAccounts () {
         return null;  // Stub
     }
-
+    
+    @Test
+    public void
+    customerAccount n = new customerAccount();
+	n.setcustomerAccount(1);
+	Assert.assertEquals(1, n.getcustomerAccount());
     /** Returns the total fees (including penalties) paid by this customer
      *  for year-to-date
      *
@@ -76,14 +105,26 @@ public class Customer implements Comparable<Customer> {
     public double ytdFees () {
         return 0.0;  // Stub
     }
-
+    
+    @Test
+    public void ytdFees() {
+    	fees a = new fees();
+    	a.setFees(0.0);
+    	Assert.assertEquals(0.0, a.getFees());	
+    }
     /** Returns the total interest paid to this customer for year-to-date
      * @return YTD interest payed
      */
     public double ytdInterest () {
         return 0.0;  // Stub
     }
-
+    
+    @Test
+    public void ytdInterest{
+    	interest a = new interest();
+    	a.setInterest(0.0);
+    	Assert.assertEquals(0.0, a.getFee());
+    }
     /** Adds a new bank account
      * @param initBal Initial balance
      * @param desc A description for the account, chosen by the customer
@@ -91,8 +132,7 @@ public class Customer implements Comparable<Customer> {
      */
     public SavingsAccount addSavingsAccount (double initBal,  String desc) {
         return null;  // Stub
-    }
-
+    }    
     /** Deletes a given account (in the real world, just marks it as defunct
      *  or something)
      * @param accountId the ID of the account to remove
@@ -101,6 +141,12 @@ public class Customer implements Comparable<Customer> {
         // Stub
     }
 
+    @Test
+    public void removeAccount() {
+    	String description = "";
+    	assertEquals("", expected);
+    	
+    }
     /** Find an account given an account ID
      *
      * @param accountId The ID of the desired account
@@ -108,6 +154,12 @@ public class Customer implements Comparable<Customer> {
      */
     public Account getAccount (String accountId) {
         return null;  // Stub
+    }
+    
+    @Test
+    public void getAccount() {
+    	String description = "";
+    	assertEquals("", accountId);
     }
 
     @Override
